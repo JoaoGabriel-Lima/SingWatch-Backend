ed292804f4d82e31b691112b42da098ec6a1b454
João Lima <jg.limamarinho202@gmail.com>
2022-03-11 23:52:20 -0300

    :sparkles:

---
+++ src/server.js
@@ -0,0 +1,63 @@
+const app = require("express")();
+const http = require("http").Server(app);
+import main from "../lib/mongodb";
+const PORT = 8080;
+const io = require("socket.io")(http);
+
+app.get("/", async (req, res) => {
+  return res.send("Singwatch API");
+});
+
+var dataRequested = false;
+let db;
+async function connectToDatabase() {
+  if (db != undefined) {
+    return db;
+  }
+  if (dataRequested == false) {
+    dataRequested = true;
+    db = await main();
+    dbTrack();
+  } else {
+    await new Promise((resolve) => setTimeout(resolve, 400));
+  }
+}
+
+async function dbTrack() {
+  const collection = db.collection("discord");
+  const changeStream = collection.watch();
+  changeStream.on("change", (next) => {
+    setNewData();
+  });
+}
+
+async function setNewData() {
+  const data = await getInitialData();
+  io.local.emit("setNewData", data);
+}
+
+async function getInitialData() {
+  await connectToDatabase();
+
+  const data = await db.collection("discord").find({}).toArray();
+  if (data) {
+    return data;
+  }
+}
+io.on("connection", async function (socket) {
+  console.log("A user connected: " + socket.id);
+
+  // if dataRequested == false, make function only imit data when db is ready
+  let data = await getInitialData();
+  console.log(data[2].channels[0]);
+  socket.emit("previusData", data);
+  //Whenever someone disconnects this piece of code executed
+  socket.on("disconnect", function () {
+    console.log("A user disconnected");
+  });
+});
+
+http.listen(PORT, async () => {
+  console.log(`Listening on port ${PORT}`);
+  connectToDatabase();
+});
